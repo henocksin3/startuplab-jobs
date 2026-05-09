570d078c3dca6b47a05b7ca1cf2bfd2b67810edf
Henock Sintaye Abeba <henock@Mac.lan>
2026-05-09 12:09:58 +0200

    DB: lazy connection init so next build doesn't require DATABASE_URL

--- src/lib/db/index.ts
+++ src/lib/db/index.ts
@@ -2,9 +2,21 @@ import { drizzle } from "drizzle-orm/postgres-js";
 import postgres from "postgres";
 import * as schema from "./schema";
 
-const url = process.env.DATABASE_URL;
-if (!url) throw new Error("DATABASE_URL is not set");
+let _db: ReturnType<typeof drizzle> | null = null;
 
-const client = postgres(url, { max: 5, prepare: false });
-export const db = drizzle(client, { schema });
+function getDb() {
+  if (_db) return _db;
+  const url = process.env.DATABASE_URL;
+  if (!url) throw new Error("DATABASE_URL is not set");
+  const client = postgres(url, { max: 5, prepare: false });
+  _db = drizzle(client, { schema });
+  return _db;
+}
+
+export const db = new Proxy({} as ReturnType<typeof drizzle>, {
+  get(_t, prop) {
+    return Reflect.get(getDb() as object, prop);
+  },
+});
+
 export { schema };
